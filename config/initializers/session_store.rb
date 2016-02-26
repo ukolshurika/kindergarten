4ad3617fa78d8bc07b1fc59bdb1586ece61c5ddc
uav14 <avukolova@mephi.ru>
2016-02-26 19:06:21 +0300

    INIT new rails app (without layouts!)

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
+
+require 'rack-cas/session_store/rails/active_record'
+Rails.application.config.session_store :rack_cas_active_record_store
